d859403af0a90ab9ba651f963f331f8ac9fa4e8d
Luis Oliveira <luispedroliveira96@gmail.com>
2023-01-18 16:02:03 +0000

    feat(main): update main.ts

--- src/main.ts
+++ src/main.ts
@@ -58,7 +58,6 @@ async function bootstrap() {
     Logger.log(`GraphQL at http://localhost:${port}/graphql`);
     // Logger.log(`Api Doc: http://localhost:${port}/${globalPrefix}/api-docs`);
     Logger.log(`Running in ${config.get('environment')} mode`);
-    Logger.log('Teste Space');
   });
 }
 
